3c8bca4117d54a571e1861b47405b28c5a9dd53e
shrestha-suvha <shrestha.suvha@gmail.com>
2023-03-10 19:33:50 +0545

    scrollbar design fix

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { FiSearch } from 'react-icons/fi';
 import { BiChevronLeft, BiChevronRight } from 'react-icons/bi';
 import { CgCompressRight } from 'react-icons/cg';
 import { BsThreeDotsVertical } from 'react-icons/bs';
+
 import Tab from 'react-bootstrap/Tab';
 import Tabs from 'react-bootstrap/Tabs';
 import Form from 'react-bootstrap/Form';
@@ -14,13 +15,10 @@ import DataList from './components/DataList';
 
 function App() {
   return (
-    <Container fluid>
+    <Container fluid className="">
       <Row className="top-nav ps-4 pe-5 d-flex justify-content-start align-items-center border-bottom">
         <Col className="ps-0 ">
-          <div
-            style={{ backgroundColor: '#eaf3ff', height: '24px', width: '24px' }}
-            className="rounded-2 d-inline-flex  justify-content-center  align-items-center "
-          >
+          <div className="rounded-2 d-inline-flex  justify-content-center  align-items-center top-nav__back-arrow">
             <BiChevronLeft size="1em" color="#1276ff" />
           </div>
           <span className="top-nav__title fw-bold fs-4 line-height-lg ps-2">Sample data 1</span>
@@ -45,33 +43,31 @@ function App() {
             <div className="d-flex  justify-content-between  px-1 py-1 side_nav__item--active">
               <div className="ms-2">
                 <AiFillHome size="1.1em" color="#1276ff" className="d-inline" />
-                {/* <div className="fs-esm d-inline ps-2"> */}
                 <span className="fw-bold text-info fs-esm ps-2">Sample data 1</span>
                 <div className="fs-esm ps-4 mt-1 text-muted">105 K records</div>
-                {/* </div> */}
               </div>
               <div className="pt-2 me-2">
                 <BsThreeDotsVertical color="gray" className="side_nav__icon--active " />
               </div>
             </div>
-            <div className="d-flex  justify-content-between align-items-center border-bottom pt-3 pb-2">
+            <div className="d-flex  justify-content-between align-items-center border-bottom pb-2 mt-4">
               <span>
-                <BiChevronRight size={'1em'} className={'side_nav__icon--expand'} />
-                <span className="side_nav__item-title ps-1">Sample data</span>
+                <BiChevronRight size={'1em'} className="side_nav__icon--expand" />
+                <span className="side_nav__item-title ps-1 ">Sample data</span>
               </span>
 
               <AiOutlinePlus className="side_nav__icon--add" />
             </div>
             <div className="side_nav__item--empty d-flex flex-column justify-content-center align-items-center">
               <div>
-                <HiDatabase className="side_nav__icon--empty" />{' '}
+                <HiDatabase className="side_nav__icon--empty" />
               </div>
               <div className="pt-2">No sample data</div>
             </div>
           </div>
         </Col>
 
-        <Col className="border-start">
+        <Col className="border-start ">
           <Row>
             <Col>
               <Tabs variant="pills" defaultActiveKey="tableOne" transition={false} id="data-table-detail">
@@ -105,16 +101,10 @@ function App() {
             <Col className=" d-flex justify-content-end ">
               <Row>
                 <Col className="align-item-center ">
-                  <div
-                    style={{ backgroundColor: '#F3F4F8', height: '32px', width: '32px' }}
-                    className="rounded-2 d-inline-flex  justify-content-center  align-items-center "
-                  >
+                  <div className=" content--icon rounded-2 d-inline-flex  justify-content-center  align-items-center ">
                     <FiSearch color="gray" />
                   </div>
-                  <div
-                    style={{ backgroundColor: '#F3F4F8', height: '32px', width: '32px', marginLeft: '8px' }}
-                    className="rounded-2 d-inline-flex  justify-content-center  align-items-center "
-                  >
+                  <div className="rounded-2 d-inline-flex  justify-content-center  align-items-center content--icon ms-2 me-4 ">
                     <CgCompressRight size={18} color="gray" />
                   </div>
                 </Col>
@@ -126,11 +116,7 @@ function App() {
               <Col className="input-group input-group-sm p-2  d-block">
                 <span className="input-group-text bg-transparent">
                   <input className="form-check-input " type="checkbox" value="table" name="checkbox" />
-                  <label
-                    className="form-check-label"
-                    style={{ marginLeft: '8px', paddingRight: '4px' }}
-                    htmlFor="flexRadioDefault1"
-                  >
+                  <label className="form-check-label input-label" htmlFor="flexRadioDefault1">
                     Table
                   </label>
                 </span>
@@ -139,7 +125,7 @@ function App() {
               <Col className="input-group input-group-sm p-2  d-block">
                 <span className="input-group-text bg-transparent">
                   <input className="form-check-input " type="checkbox" value="schema" name="checkbox" checked />
-                  <label className="form-check-label" style={{ marginLeft: '8px' }} htmlFor="flexRadioDefault1">
+                  <label className="form-check-label input-label" htmlFor="flexRadioDefault1">
                     Schema
                   </label>
                 </span>
@@ -147,7 +133,7 @@ function App() {
               <Col className="input-group input-group-sm p-2  d-block ">
                 <span className="input-group-text bg-transparent">
                   <input className="form-check-input" type="checkbox" value="option3" name="checkbox" />
-                  <label className="form-check-label" style={{ marginLeft: '8px' }} htmlFor="flexRadioDefault1">
+                  <label className="form-check-label input-label" htmlFor="flexRadioDefault1">
                     Option 3
                   </label>
                 </span>
--- src/components/DataList/index.tsx
+++ src/components/DataList/index.tsx
@@ -2,14 +2,19 @@ import React, { FC } from 'react';
 import { Row, Container, Col } from 'react-bootstrap';
 import Table from 'react-bootstrap/Table';
 import { FaHashtag } from 'react-icons/fa';
-import { AiOutlineInfoCircle } from 'react-icons/ai';
-import { BiFontColor } from 'react-icons/bi';
+import { AiOutlineInfoCircle, AiOutlineFontColors } from 'react-icons/ai';
 import ProgressBar from 'react-bootstrap/ProgressBar';
 
 const tableData = [
-  { name: 0, description: 'Available' },
-  { name: 1, description: 'Ready' },
-  { name: 2, description: 'Started' },
+  { name: 'Geo Id', description: 'Available' },
+  { name: 'Zip Code', description: 'Ready' },
+  { name: 'Contact', description: 'Started' },
+  { name: 'Geo Id', description: 'Available' },
+  { name: 'Zip Code', description: 'Ready' },
+  { name: 'Contact', description: 'Started' },
+  { name: 'Geo Id', description: 'Available' },
+  { name: 'Zip Code', description: 'Ready' },
+  { name: 'Contact', description: 'Started' },
 ];
 
 const DataList: FC = () => {
@@ -17,70 +22,75 @@ const DataList: FC = () => {
     <Container>
       <Row>
         <Col>
-          <Table striped borderless responsive>
-            <thead>
-              <tr>
-                <th>
-                  <FaHashtag style={{ color: 'gray', marginRight: '4px', fontSize: 16 }} />
-                  <span className="fs-md fw-bold">geo_name</span>
-                  <AiOutlineInfoCircle style={{ marginLeft: '4px' }} />
-                </th>
-                <th>
-                  <BiFontColor style={{ marginLeft: '4px', color: 'gray', fontSize: 16 }} />
-                  <span className="fs-md fw-bold">location</span>
-                  <AiOutlineInfoCircle style={{ marginLeft: '4px' }} />
-                </th>
-                <th>
-                  <FaHashtag style={{ color: 'gray', marginRight: '4px', fontSize: 16 }} />
-                  <span className="fs-md fw-bold">contact_no</span>
-                  <AiOutlineInfoCircle style={{ marginLeft: '4px' }} />
-                </th>
-              </tr>
-            </thead>
-            <tbody className="ft-table fs-md">
-              {tableData?.map(item => (
-                <tr key={item?.name}>
-                  <td className="fs-sm">{item?.name}</td>
-                  <td className="fs-sm">{item?.name}</td>
-                  <td className="fs-sm">{item?.name}</td>
+          <div className="table-wrapper">
+            <Table striped borderless className="fs-sm" style={{ maxHeight: '200px' }}>
+              <thead>
+                <tr>
+                  <th>
+                    <FaHashtag className="table__icon--hash " />
+                    <span className="fw-bold">geo_name</span>
+                    <AiOutlineInfoCircle className="table__icon--info" />
+                  </th>
+                  <th>
+                    <AiOutlineFontColors className="table__icon--hash " />
+                    <span className="fw-bold">location</span>
+                    <AiOutlineInfoCircle className="table__icon--info" />
+                  </th>
+                  <th>
+                    <FaHashtag className="table__icon--hash" />
+                    <span className="fw-bold">contact_no</span>
+                    <AiOutlineInfoCircle className="table__icon--info" />
+                  </th>
                 </tr>
-              ))}
-            </tbody>
-          </Table>
+              </thead>
+              <tbody className="fs-sm">
+                {tableData?.map(item => (
+                  <tr key={item?.name}>
+                    <td>{item?.name}</td>
+                    <td>{item?.name}</td>
+                    <td>{item?.name}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </Table>
+          </div>
         </Col>
+
         <Col>
-          <Table responsive style={{ maxHeight: '10px' }}>
-            <thead>
-              <tr>
-                <th className="table-header">Column name</th>
-                <th className="table-header">Description</th>
-                <th></th>
-              </tr>
-            </thead>
-            <tbody className="ft-table">
-              {tableData?.map(item => (
-                <tr key={item?.name}>
-                  <td>
-                    <Row>
-                      <Col>
-                        <span className="">{item?.name}</span>
-                        <p className="text-muted"># Integer</p>
-                      </Col>
-                    </Row>
-                  </td>
-                  <td>
-                    <Row>
-                      <Col>
-                        <div style={{ color: '#1B2F50' }}>{item?.description}</div>
-                        <span className="fill-rate">Fillrate :80%</span>
-                        <ProgressBar now={80} visuallyHidden style={{ height: '2px', marginTop: '4px' }} />
-                      </Col>
-                    </Row>
-                  </td>
+          <div className="table-wrapper border rounded-1">
+            <Table className="fs-esm ">
+              <thead>
+                <tr>
+                  <th className="table-header">Column name</th>
+                  <th className="table-header">Description</th>
                 </tr>
-              ))}
-            </tbody>
-          </Table>
+              </thead>
+              <tbody className="">
+                {tableData?.map(item => (
+                  <tr key={item?.name}>
+                    <td>
+                      <Row>
+                        <Col>
+                          <span className="">{item?.name}</span>
+                          <p className="text-muted"># Integer</p>
+                        </Col>
+                      </Row>
+                    </td>
+                    <td>
+                      <Row>
+                        <Col>
+                          <div style={{ color: '#1B2F50' }}>{item?.description}</div>
+                          <span className="fill-rate">Fillrate :10%</span>
+
+                          <ProgressBar now={70} visuallyHidden className="bg-transparent" />
+                        </Col>
+                      </Row>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </Table>
+          </div>
         </Col>
       </Row>
     </Container>
